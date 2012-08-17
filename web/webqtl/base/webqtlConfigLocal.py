724ed0afa9184692e2e218ece6a3e87c52d7cd40
root <root@alexandria.uthsc.edu>
2012-08-17 14:49:27 -0500

    GeneNetwork configuration for Lei Yan
     Committer: root <root@alexandria.uthsc.edu>
    
     On branch leiyan
     Changes to be committed:
    	modified:   web/webqtl/.htaccess
    	modified:   web/webqtl/base/webqtlConfigLocal.py

--- web/webqtl/base/webqtlConfigLocal.py
+++ web/webqtl/base/webqtlConfigLocal.py
@@ -3,15 +3,15 @@
 #########################################
 
 MYSQL_SERVER = 'localhost'
-DB_NAME = 'db_webqtl'
+DB_NAME = 'db_webqtl_leiyan'
 DB_USER = 'webqtl'
 DB_PASSWD = 'webqtl'
 
 MYSQL_UPDSERVER = 'localhost'
-DB_UPDNAME = 'db_webqtl'
+DB_UPDNAME = 'db_webqtl_leiyan'
 DB_UPDUSER = 'webqtl'
 DB_UPDPASSWD = 'webqtl'
 
-GNROOT = '/gnshare/gn/'
+GNROOT = '/home/leiyan/gn/'
 PythonPath = '/usr/bin/python'
 PIDDLE_FONT_PATH = '/usr/lib/python2.4/site-packages/piddle/truetypefonts/'
