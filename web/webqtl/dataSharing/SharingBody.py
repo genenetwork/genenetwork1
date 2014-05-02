928f70508fa927af468abd22647f03da88ff9488
root <root@spring211.uthsc.edu>
2014-05-01 23:54:24 -0500

    Committer: root <root@spring211.uthsc.edu> On branch master

--- web/webqtl/dataSharing/SharingBody.py
+++ web/webqtl/dataSharing/SharingBody.py
@@ -130,10 +130,10 @@ sharing_body_string = """
 # Note by AC: List of %s variables are listed in SharingInfo2.py 
 sharinginfo_body_string = """<td>
 
-<a href="/webqtl/main.py?FormID=sharingListDataset">List of DataSets</a><br>
+<!--<a href="/webqtl/main.py?FormID=sharingListDataset">List of DataSets</a><br>-->
 
 <H1 class="title" id="parent-fieldname-title">%s <!--info[9] Datasets.DatasetName-->
-<a href="/webqtl/main.py?FormID=sharinginfoedit&InfoPageName=%s"><img src="/images/modify.gif" alt="modify this page" border="0" valign="middle"></a>
+<!--<a href="/webqtl/main.py?FormID=sharinginfoedit&InfoPageName=%s"><img src="/images/modify.gif" alt="modify this page" border="0" valign="middle"></a>-->
 <span style="color:red;">%s</span> <!--info[0] InfoPageName-->
 </H1>
 <table border="0" width="100%%">
