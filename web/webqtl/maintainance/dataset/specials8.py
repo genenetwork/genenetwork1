6a55f4d366a9f9fc62856d24526b5da8508de7f6
Lei Yan <lyan6@uthsc.edu>
2014-10-13 14:45:16 -0500

    On branch develop

--- web/webqtl/maintainance/dataset/specials8.py
+++ web/webqtl/maintainance/dataset/specials8.py
@@ -1,6 +1,7 @@
 import sys
 
 import datastructure
+import probesets
 
 """
 For:	Ash
@@ -13,20 +14,36 @@ def traverse(outputfile):
 	#
 	file = open(outputfile, 'w')
 	inbredsetid = 1
-	#
+	strains = datastructure.get_strains(inbredsetid)
+	print("strains: %s" % len(strains))
 	probesetfreezes = datastructure.get_probesetfreezes(inbredsetid)
-	print "From DB, get %d probesetfreezes" % (len(probesetfreezes))
+	print("probesetfreezes: %s" % len(probesetfreezes))
+	#
+	file.write("DatasetID\t")
+	file.write("DatasetName\t")
+	file.write("RecordNumber\t")
+	for strain in strains:
+		file.write("%s\t" % strain[1])
+	file.write("\n")
+	file.flush()
 	#
 	for probesetfreeze in probesetfreezes:
 		#
-		print probesetfreeze
 		probesetfreezeid = probesetfreeze[0]
 		probesetfreezename = probesetfreeze[1]
 		probesetfreezefullname = probesetfreeze[2]
+		probesetxrefs = probesets.get_probesetxref(probesetfreezeid)
 		#
+		file.write("%d\t" % probesetfreezeid)
+		file.write("%s\t" % probesetfreezefullname)
+		file.write("%d\t" % len(probesetxrefs))
+		#
+		file.write("\n")
 		file.flush()
 		#
 	file.close()
+
+#  python specials8.py /home/leiyan/datadir/20140205_Ash_BXD/statistic/trait_numbers.txt
 
 if __name__ == "__main__":
 	print("command line arguments:\n\t%s" % sys.argv)
